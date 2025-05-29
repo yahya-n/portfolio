d0e8ec76868553c23d6301d0754700da9036e040
Yahya <abdulkuddoosyahya@gmail.com>
2025-05-30 02:12:39 +0530

    Add gevent and greenlet test files and update requirements

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
 django.setup()
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]

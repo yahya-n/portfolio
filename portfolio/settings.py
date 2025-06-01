995a46fad2de3388259f91b3e48c6ae024052f67
Yahya <abdulkuddoosyahya@gmail.com>
2025-06-01 18:37:52 +0530

    Refactor DEBUG and ALLOWED_HOSTS settings to use environment variables for better configuration management

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1"]
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+
+ALLOWED_HOSTS = [host.strip() for host in os.getenv("ALLOWED_HOSTS", "").split(",") if host.strip()]
 
 # Application definition
 INSTALLED_APPS = [
